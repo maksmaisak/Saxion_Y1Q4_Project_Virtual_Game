880980e7a07a927c9616caffce42afa341fadaf3
Maks <max.maisak@gmail.com>
2018-05-08 01:51:26 +0200

    Started work on grappling hooks for the player

---
+++ Assets/_Own/Scripts/Player/Grapple.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityStandardAssets.Characters.FirstPerson;
+
+#pragma warning disable 0649
+
+/// Creates a physics joint between itself and origin upon making contact.
+[RequireComponent(typeof(Rigidbody), typeof(LineRenderer))]
+public class Grapple : MonoBehaviour {
+
+    enum State {
+        
+        Retracted,
+        Flying,
+        Connected
+    }
+
+    [SerializeField] Transform attachmentPoint;
+    [SerializeField] Rigidbody attachmentRigidbody;
+
+    private new Rigidbody rigidbody;
+    private RigidbodyFirstPersonController firstPersonController;
+    private LineRenderer lineRenderer;
+
+    private State state = State.Retracted;
+    private SpringJoint joint;
+
+    public bool isRetracted {
+        get { return state == State.Retracted; }
+    }
+
+    void Start() {
+
+        rigidbody = GetComponent<Rigidbody>();
+        firstPersonController = GetComponentInParent<RigidbodyFirstPersonController>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update() {
+
+        if (isRetracted) {
+            transform.position = attachmentPoint.position;
+        }
+
+        lineRenderer.SetPosition(0, attachmentPoint.position);
+        lineRenderer.SetPosition(1, rigidbody.position);
+    }
+
+    void FixedUpdate() {
+
+        if (joint != null) {
+            
+            float ropeLength = joint.minDistance;
+            float currentDistance = Vector3.Distance(attachmentRigidbody.position, joint.connectedBody.position);
+
+            if (currentDistance < ropeLength) {
+                ropeLength = currentDistance;
+            } else if (ropeLength > 0f && !firstPersonController.Grounded) {
+
+                ropeLength -= 1f * Time.fixedDeltaTime;
+                if (ropeLength < 0f) ropeLength = 0f;
+            }
+
+            joint.minDistance = joint.maxDistance = ropeLength;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+        if (state != State.Flying) return;
+
+        // Fix in place
+        rigidbody.isKinematic = true;
+        transform.position = collision.contacts[0].point;
+        transform.SetParent(collision.transform, worldPositionStays: true);
+
+        // Create the joint
+        var targetRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        if (targetRigidbody == null) targetRigidbody = rigidbody;
+
+        joint = attachmentRigidbody.gameObject.AddComponent<SpringJoint>();
+        float currentDistance = Vector3.Distance(attachmentRigidbody.position, targetRigidbody.position);
+        joint.minDistance = joint.maxDistance = currentDistance;
+        joint.autoConfigureConnectedAnchor = false;
+        joint.anchor = Vector3.zero;
+        joint.connectedAnchor = Vector3.zero;
+        joint.spring = 1000f;
+        joint.connectedBody = targetRigidbody;
+
+        // Change state
+        state = State.Connected;
+    }
+
+    public void Shoot(Vector3 startVelocity) {
+
+        if (!isRetracted) Retract();
+
+        transform.SetParent(null, worldPositionStays: true);
+
+        rigidbody.isKinematic = false;
+        rigidbody.velocity = startVelocity;
+
+        state = State.Flying;
+    }
+
+    public void Retract() {
+
+        if (state == State.Retracted) return;
+
+        if (joint != null) {
+
+            Destroy(joint);
+            joint = null;
+        }
+
+        rigidbody.isKinematic = true;
+
+        transform.SetParent(attachmentPoint, worldPositionStays: false);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        state = State.Retracted;
+    }
+}
---
+++ Assets/_Own/Scripts/Player/GrappleController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+#pragma warning disable 0649
+
+public class GrappleController : MonoBehaviour {
+
+    [SerializeField] Grapple grappleLeft;
+    [SerializeField] Grapple grappleRight;
+
+    [SerializeField] float grappleShootSpeed = 100f;
+
+    void Start() {
+
+        Assert.IsNotNull(grappleLeft);
+        Assert.IsNotNull(grappleRight);
+    }
+
+    void Update() {
+
+        if (Input.GetButtonDown("Fire1")) {
+            grappleLeft.Shoot(Camera.main.transform.forward * grappleShootSpeed);
+        } 
+
+        if (Input.GetButtonUp("Fire1")) {
+            grappleLeft.Retract();
+        }
+
+        if (Input.GetButtonDown("Fire2")) {
+            grappleRight.Shoot(Camera.main.transform.forward * grappleShootSpeed);
+        }
+
+        if (Input.GetButtonUp("Fire2")) {
+            grappleRight.Retract();
+        }
+    }
+}
