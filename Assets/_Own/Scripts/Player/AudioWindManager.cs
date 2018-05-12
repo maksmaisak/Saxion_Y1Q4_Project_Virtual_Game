cb40584c8ec3421ded3fabb44b02c86f125f6d0e
Maks <max.maisak@gmail.com>
2018-05-12 17:38:23 +0200

    Added basic wind sound when moving fast

---
+++ Assets/_Own/Scripts/Player/AudioWindManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class AudioWindManager : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Rigidbody rigidbodyToTrack;
+    [Space]
+    [SerializeField] float minSpeed = 1f;
+    [SerializeField] float maxSpeed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Assert.IsNotNull(audioSource);
+        Assert.IsNotNull(rigidbodyToTrack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = GetDesiredVolume();
+    }
+
+    private float GetDesiredVolume()
+    {
+        float speed = rigidbodyToTrack.velocity.magnitude;
+        return Mathf.Clamp01(Mathf.InverseLerp(minSpeed, maxSpeed, speed));
+    }
+}
