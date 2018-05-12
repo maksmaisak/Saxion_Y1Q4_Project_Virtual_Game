5c1fb2489262696306d45a557beb4d692a07ef96
Maks <max.maisak@gmail.com>
2018-05-12 17:44:20 +0200

    Minor

--- Assets/_Own/Scripts/Player/AudioWindManager.cs
+++ Assets/_Own/Scripts/Player/AudioWindManager.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+/// Adjusts the volume of a given audioSource to play a wind sound when moving fast.
 public class AudioWindManager : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
     [SerializeField] Rigidbody rigidbodyToTrack;
     [Space]
+    [Tooltip("Volume is 0 when moving slower than this.")]
     [SerializeField] float minSpeed = 1f;
+    [Tooltip("Volume is 1 when moving faster than this.")]
     [SerializeField] float maxSpeed = 10f;
 
     // Use this for initialization
@@ -17,6 +20,9 @@ public class AudioWindManager : MonoBehaviour
     {
         Assert.IsNotNull(audioSource);
         Assert.IsNotNull(rigidbodyToTrack);
+
+        audioSource.loop = true;
+        if (!audioSource.isPlaying) audioSource.Play();
     }
 
     // Update is called once per frame
