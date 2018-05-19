ba38a0ed19e728eb7a32c183f70c50f6d0e3be9f
Maks <max.maisak@gmail.com>
2018-05-19 16:19:23 +0200

    Fixed effects getting destroyed before their audio clips stop playing.

--- Assets/_Own/Scripts/Explosion/AutoDestroyParticleSystem.cs
+++ Assets/_Own/Scripts/Explosion/AutoDestroyParticleSystem.cs
@@ -3,14 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoDestroyParticleSystem : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-
-        float duration = GetComponentsInChildren<ParticleSystem>()
-            .Max(ps => ps.main.duration);
+public class AutoDestroyParticleSystem : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        float duration = Mathf.Max(GetParticlesDuration(), GetAudioDuration());
 
         Destroy(gameObject, duration);
-	}
+    }
+
+    private float GetParticlesDuration()
+    {
+        ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
+        if (particleSystems.Length == 0) return 0f;
+        return particleSystems.Max(ps => ps.main.duration);
+    }
+
+    private float GetAudioDuration()
+    {
+        AudioSource[] audioSources = GetComponentsInChildren<AudioSource>();
+        if (audioSources.Length == 0) return 0f;
+        return audioSources.Max(source => source.clip.length);
+    }
 }
