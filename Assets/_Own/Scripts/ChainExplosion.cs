f8047134f0592c6a5d8cbb3557193de35b20b395
Igli milaqi <milaqiigli@gmail.com>
2018-05-21 20:59:29 +0200

    Added torque to chains.

--- Assets/_Own/Scripts/ChainExplosion.cs
+++ Assets/_Own/Scripts/ChainExplosion.cs
@@ -7,6 +7,7 @@ public class ChainExplosion : MonoBehaviour
     [SerializeField] private float explosionForce = 5;
     [SerializeField] private float explosionRadius = 8;
     [SerializeField] private float upwardsModifier = 3;
+    [SerializeField] private float torque = 7;
     private Rigidbody rb;
    
     public void ExplodeChains()
@@ -18,6 +19,7 @@ public class ChainExplosion : MonoBehaviour
                 child.gameObject.AddComponent<Rigidbody>();
                 rb = child.GetComponent<Rigidbody>();
                 rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, upwardsModifier, ForceMode.Impulse);
+                rb.AddTorque(transform.up * torque,ForceMode.Impulse);
                 Destroy(child.gameObject, 2);
             }
         }
