90cec32df8d2a031878397aa559195070b3c66cd
Maks <max.maisak@gmail.com>
2018-05-23 16:56:59 +0200

    minor

---
+++ Assets/_Own/Scripts/TutorialHints/GrappleHint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0649
+
+public class GrappleHint : TutorialHint
+{
+    [SerializeField] bool leftOrRight;
+
+    protected override bool CheckTransitionOutCondition()
+    {
+        return Input.GetButtonDown(leftOrRight ? "Fire1" : "Fire2");
+    }
+}
