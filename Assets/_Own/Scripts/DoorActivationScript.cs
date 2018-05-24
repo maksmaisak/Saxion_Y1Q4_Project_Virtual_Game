d4938ccce21b48d74b02d0673495663b994e9c03
Igli milaqi <milaqiigli@gmail.com>
2018-05-24 15:26:12 +0200

    Added door rotation scripts.

---
+++ Assets/_Own/Scripts/DoorActivationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorActivationScript : MonoBehaviour
+{
+    [SerializeField] private float maxDegreesPerSecond= 2;
+    [HideInInspector] public bool isActivated = false;
+
+    void Start()
+    {
+        //Activate();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            isActivated = true;
+        }
+
+        if(isActivated)
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+
+        foreach (Transform child in transform)
+        {
+            child.transform.rotation = Quaternion.RotateTowards(child.transform.rotation,Quaternion.identity,maxDegreesPerSecond * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/_Own/Scripts/DoorPieceRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPieceRotation : MonoBehaviour
+{
+
+    [SerializeField] private float maxRotationSpeed = 10;
+    [SerializeField] private float minRotationSpeed = -10;
+
+    private float rotationSpeed = 0;
+    private DoorActivationScript door;
+
+    void Start()
+    {
+        rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        door = transform.parent.GetComponent<DoorActivationScript>();
+    }
+
+
+    void Update()
+    {
+        if (door.isActivated == false)
+        {
+            RotateOverTime();
+        }
+    }
+
+    void RotateOverTime()
+    {
+        transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * rotationSpeed);
+    }
+}
