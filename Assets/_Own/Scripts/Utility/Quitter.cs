bcc603839f4f1aedf1ecbd8936adb3209cdbd07f
Maks <max.maisak@gmail.com>
2018-05-25 06:38:22 +0200

    Baked lighting

---
+++ Assets/_Own/Scripts/Utility/Quitter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quitter : MonoBehaviour
+{
+    public void QuitToDesktop() 
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+}
